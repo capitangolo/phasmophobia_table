f58f90af693b5b3ef3b81295178eabe14417c428
Víctor Jiménez Cerrada <vjimenez@rootnode.es>
2021-05-30 15:15:14 +0200

    [feat] Initial list of ghosts

--- src/App.js
+++ src/App.js
@@ -2,57 +2,236 @@ import React, { Component } from 'react';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      data:[
-            {
-              "name":"Sombra"
-            },
-            {
-              "name":"Ente"
-            },
-            {
-              "name":"Oni"
-            }
-        ]
+
+    this.Evidences = {
+      emf: {
+        name: "EMF Level 5",
+        value: 1
+      },
+      fingerprints: {
+        name: "Fingerprints",
+        value: 2
+      },
+      temp: {
+        name: "Freezing Temperatures",
+        value: 4
+      },
+      orbs: {
+        name: "Ghost Orb",
+        value: 8
+      },
+      writing: {
+        name: "Ghost Writing",
+        value: 16
+      },
+      box: {
+        name: "Spirit Box",
+        value: 32
+      },
+      footprints: {
+        name: "Footprints",
+        value: 64
+      }
     }
-    this.addGhost = this.addGhost.bind(this);
+    this.Ghosts = [
+      {
+        name: "Banshee",
+        evidences: this.Evidences.emf.value
+                 + this.Evidences.fingerprints.value
+                 + this.Evidences.temp.value
+                 + this.Evidences.footprints.value
+
+      },
+      {
+        name: "Demon",
+        evidences: this.Evidences.temp.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.temp.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Jinn",
+        evidences: this.Evidences.emf.value
+                 + this.Evidences.orbs.value
+                 + this.Evidences.box.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Mare",
+        evidences: this.Evidences.temp.value
+                 + this.Evidences.orbs.value
+                 + this.Evidences.box.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Oni",
+        evidences: this.Evidences.emf.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.box.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Phantom",
+        evidences: this.Evidences.emf.value
+                 + this.Evidences.temp.value
+                 + this.Evidences.orbs.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Poltergeist",
+        evidences: this.Evidences.fingerprints.value
+                 + this.Evidences.orbs.value
+                 + this.Evidences.box.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Revenant",
+        evidences: this.Evidences.emf.value
+                 + this.Evidences.fingerprints.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Shade",
+        evidences: this.Evidences.emf.value
+                 + this.Evidences.orbs.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Spirit",
+        evidences: this.Evidences.fingerprints.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.box.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Wraith",
+        evidences: this.Evidences.fingerprints.value
+                 + this.Evidences.temp.value
+                 + this.Evidences.box.value
+      },
+      {
+        name: "Yurei",
+        evidences: this.Evidences.temp.value
+                 + this.Evidences.orbs.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Yokai",
+        evidences: this.Evidences.orbs.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.box.value
+                 + this.Evidences.footprints.value
+      },
+      {
+        name: "Hantu",
+        evidences: this.Evidences.fingerprints.value
+                 + this.Evidences.orbs.value
+                 + this.Evidences.writing.value
+                 + this.Evidences.footprints.value
+      }
+    ]
+
+    this.state = {
+      evidences_yes: 0,
+      evidences_not: 0,
+      possible_ghosts: this.Ghosts
+    }
+
+    this.toggleEvidence = this.toggleEvidence.bind(this);
   }
 
-  addGhost(){
-    var newdata = this.state.data
-    newdata.push({ "name":"Danifalky" })
-    this.setState({data: newdata});
+  toggleEvidence(evidence){
+    // evidence = 0010
+    // value    = 0111
+    // ----------------
+    // and      = 0010
+    var has_evidence = this.state.evidences_yes & evidence == evidence
+    var not_evidence = this.state.evidences_not & evidence == evidence
+    var new_evidences_yes = this.state.evidences_yes
+    var new_evidences_not = this.state.evidences_not
+
+    if ( has_evidence ) {
+      new_evidences_yes = this.state.evidences_yes - evidence
+      new_evidences_not = this.state.evidences_not + evidence
+    } else if ( not_evidence ) {
+      new_evidences_not = this.state.evidences_not - evidence
+    } else {
+      new_evidences_yes = this.state.evidences_yes + evidence
+    }
+
+    var new_possible_ghosts = this.calculateGhosts(this.Ghosts, new_evidences_yes, new_evidences_not)
+
+    this.setState({
+      evidences_yes: new_evidences_yes,
+      evidences_not: new_evidences_not,
+      possible_ghosts: new_possible_ghosts
+    });
+  }
+
+  calculateGhosts(ghosts, evidences_yes, evidences_not) {
+    return ghosts.filter( (ghost) => {
+      // evidence = 0110
+      // ghost    = 0111
+      // ----------------
+      // and      = 0110
+      var matches_yes = ghost.evidences & evidences_yes == evidences_yes
+
+      // evidence = 1001
+      // ghost    = 0111
+      // ----------------
+      // and      = 0001
+      var matches_not = ghost.evidences & evidences_not != 0
+
+      return matches_yes && !matches_not
+    })
   }
 
   render() {
     return (
       <div>
-        <ListTitle/>
-          <ul>
-            {this.state.data.map((item) => <List data = {item} />)}
-          </ul>
+      <div>
+        <h1>Possible Ghosts</h1>
+        <ul>
+          {this.state.possible_ghosts.map((item) => <Ghost ghost = {item} />)}
+        </ul>
         <button onClick={this.addGhost}> Add Ghost </button>
+      </div>
+      <div>
+        <h1>Possible Evidences</h1>
+        <ul>
+          {Object.entries(this.Evidences).map((item) => <EvidenceButton evidence={item[1]} callback={this.toggleEvidence} state={this.state} />)}
+        </ul>
+      </div>
       </div>
     );
   }
 }
 
-class ListTitle extends React.Component {
+class Ghost extends React.Component {
    render() {
       return (
-         <div>
-            <h1>Possible Ghosts</h1>
-         </div>
+        <li>{this.props.ghost.name}</li>
       );
    }
 }
-class List extends React.Component {
-   render() {
-      return (
-         <ul>
-            <li>{this.props.data.name}</li>
-         </ul>
-      );
-   }
+
+class EvidenceButton extends Component {
+  handleClick = () => {
+    this.props.callback(this.props.evidence.value);
+  }
+
+  render() {
+    var value = this.props.evidence.value
+    var has_evidence = this.props.state.evidences_yes & value == value
+    var not_evidence = this.props.state.evidences_not & value == value
+
+
+    return (
+      <li><button style={{"background-color": "green"}} onClick={this.handleClick}>{this.props.evidence.name}</button></li>
+    );
+  }
 }
 export default App;
