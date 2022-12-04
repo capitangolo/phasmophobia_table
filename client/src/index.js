9c2b24a179611cf52f76c7e0023e1430112cca1a
Víctor Jiménez Cerrada <vjimenez@rootnode.es>
2022-12-04 17:23:14 +0100

    [feat] Removed Google Analytics. We'll implement prometheus metrics.

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import ReactGA from 'react-ga';
 
 import App from './App';
 import { IntlProviderWrapper } from "./IntlContext";
-
-if (process.env.REACT_APP_GA_TRACKING_ID !== undefined) {
-  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
--- overlay/src/index.js
+++ overlay/src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import ReactGA from 'react-ga';
 
 import OverlayApp from './OverlayApp';
-
-if (process.env.REACT_APP_GA_TRACKING_ID !== undefined) {
-  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
