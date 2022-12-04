42aededd1c1a577045f8b512826f9c654b3c42a8
Víctor Jiménez Cerrada <vjimenez@rootnode.es>
2022-12-04 16:45:11 +0100

    [feat] Added support for Google Analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import ReactGA from 'react-ga';
 
 import App from './App';
 import { IntlProviderWrapper } from "./IntlContext";
 
-ReactDOM.render(
+if (process.env.REACT_APP_GA_TRACKING_ID !== undefined) {
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <IntlProviderWrapper>
       <App />
     </IntlProviderWrapper>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
--- overlay/src/index.js
+++ overlay/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import OverlayApp from './OverlayApp';
 
-ReactDOM.render(
+if (process.env.REACT_APP_GA_TRACKING_ID !== undefined) {
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
-    <OverlayApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <IntlProviderWrapper>
+      <App />
+    </IntlProviderWrapper>
+  </React.StrictMode>
 );
